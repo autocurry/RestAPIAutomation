17685374a98ac387271fdaa17f026b86b27c2025
sandeep <sandeepsekhar555@gmail.com>
2018-12-08 10:42:02 +0800

    Initial checkin

---
+++ APIAutomation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace RestAPIAutomation
+{
+    public class APIAutomation
+    {
+        [Fact]
+        public void GetAPIAutomation()
+        {
+            Console.WriteLine("inside the first test");
+            Assert.True(true);
+        }
+    }
+}
